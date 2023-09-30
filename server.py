73e3a65541eab27050160e9beff1f5a2d0f02e50
Nazmus Safwan <112449051+nsafwan@users.noreply.github.com>
2023-09-30 15:03:33 -0400

    init

--- server.py
+++ server.py
@@ -10,17 +10,6 @@ def serve_index():
 def serve_file(resource):
     return send_from_directory('public', resource)
 
-@app.route('/visit-counter')
-def visit_cookie():
-    current_value = request.cookies.get('visits')
-    if current_value:
-        new_value = int(current_value) + 1
-    else:
-        new_value = 1
-    response = make_response(str(new_value))
-    response.set_cookie('visits', str(new_value), max_age=3600)
-    return response
-
 
 @app.after_request
 def apply_nosniff(response):
