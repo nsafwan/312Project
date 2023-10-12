315c994bd7ff38336c3b8d8580f3f061409baf8a
Corey Brown <corey.brown.ny@gmail.com>
2023-10-12 14:13:35 -0400

    Added Objective 2 (submitting posts) functionality

--- server.py
+++ server.py
@@ -31,23 +31,55 @@ user_collection.insert_one({"username": "Test User", "shpassword": "Test salted 
 
 
 
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def serve_index():
-#    return send_from_directory('public', 'index.html')
-#
-# @app.route('/public/<path:resource>')
-# def serve_file(resource):
-#     return send_from_directory('public', resource)
-#
-#
-# @app.after_request
-# def apply_nosniff(response):
-#     response.headers['X-Content-Type-Options'] = 'nosniff'
-#     return response
-#
-#
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=8080)
+app = Flask(__name__)
+
+@app.route('/')
+def serve_index():
+   return send_from_directory('public', 'index.html')
+
+@app.route('/public/<path:resource>')
+def serve_file(resource):
+    return send_from_directory('public', resource)
+
+
+@app.route("/submit-post", methods=["POST"])
+def submit_post():
+    # get title and description of post
+    post_data = request.get_json()
+    title = post_data["title"].replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+    description = post_data["description"].replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
+
+    # Check to see if user is authenticated
+    # search for auth token cookie
+    auth_token_cookie_name = "auth-token"
+    if auth_token_cookie_name in request.cookies:
+        request_auth_token = request.cookies.get(auth_token_cookie_name)
+        # hashed_request_auth_token needs to be calculated
+        hashed_request_auth_token = request_auth_token
+        user = user_collection.find_one({"auth": hashed_request_auth_token})
+        if user:
+            username = user["username"]
+            # pstnumber needs to be calculated
+            postnumber = 2
+            post_collection.insert_one({
+                "username": username,
+                "title": title,
+                "description": description,
+                "likes": 0,
+                "postnumber": postnumber
+            })
+            return "Successfully posted"
+
+    # User not authenticated. Post not submitted. Status code 401
+    return "Unauthenticated", 401
+
+
+@app.after_request
+def apply_nosniff(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
 
