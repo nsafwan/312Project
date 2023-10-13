ded10e3e12cf2051efde922b246aa6d2f8fe5798
Ethan <ejholmes@buffalo.edu>
2023-10-12 21:07:39 -0400

    Added objective 1 register and login functionality

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_from_directory, request, make_response
 from pymongo import MongoClient
+import bcrypt
+from uuid import uuid4  # used to generate auth token
 
-
-mongo_client = MongoClient("localhost") #This should be changed to mongo for docker
-db = mongo_client["cse312"] #Creating a mongo database called cse312
-
+mongo_client = MongoClient("localhost")  # This should be changed to mongo for docker
+db = mongo_client["cse312"]  # Creating a mongo database called cse312
 
 """
 Collection of all the users
@@ -15,8 +15,6 @@ liked is an array of all the postnumber that the user has liked (starts off with
 """
 user_collection = db["users"]
 
-
-
 """
 Collection of all the posts.
 Format: (username, title, description, likes, postnumber)
@@ -25,17 +23,18 @@ Likes attribute stores the total number of likes this post has.
 """
 post_collection = db["posts"]
 
-
-post_collection.insert_one({"username": "Test User", "title": "Test Title", "description": "Test description", "likes": 0, "postnumber": 1})
-user_collection.insert_one({"username": "Test User", "shpassword": "Test salted hashed password", "auth": "Test salted auth", "liked": [1]})
-
-
+post_collection.insert_one(
+    {"username": "Test User", "title": "Test Title", "description": "Test description", "likes": 0, "postnumber": 1})
+user_collection.insert_one(
+    {"username": "Test User", "shpassword": "Test salted hashed password", "auth": "Test salted auth", "liked": [1]})
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def serve_index():
-   return send_from_directory('public', 'index.html')
+    return send_from_directory('public', 'index.html')
+
 
 @app.route('/public/<path:resource>')
 def serve_file(resource):
@@ -74,6 +73,36 @@ def submit_post():
     return "Unauthenticated", 401
 
 
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.form
+    # assuming same form input names as hw2 html (username_reg and password_reg)
+    username = data["username_reg"]
+    shpassword = bcrypt.hashpw(data["password_reg"].encode(), bcrypt.gensalt())
+    existing_user = user_collection.find_one({"username": username})
+    if existing_user:
+        return "Username Taken", 400
+    else:
+        user_collection.insert_one({"username": username, "shpassword": shpassword})
+        return "New User Registered"
+
+@app.route("/login", methods=["POST"])
+def login():
+    response = make_response("User Logged In")
+    data = request.form
+    # assuming same form input names as hw2 html (username_login and password_login)
+    username = data["username_login"]
+    existing_user = user_collection.find_one({"username": username})
+    if existing_user and bcrypt.checkpw(data["password_login"].encode(), existing_user["shpassword"]):
+        auth = str(uuid4())  # generate auth token
+        response.set_cookie('auth_token', auth, httponly=True, max_age=3600)  # set cookie of un-hashed auth token
+        auth = bcrypt.hashpw(auth.encode(), bcrypt.gensalt())  # hash byte string of auth token
+        auth = {"$set": {"auth": auth}}  # create new dict element to add to database entry
+        user_collection.update_one({"username": username}, auth)  # add auth token to user entry
+        return response
+    else:
+        return "Incorrect Username or Password", 400
+
 @app.after_request
 def apply_nosniff(response):
     response.headers['X-Content-Type-Options'] = 'nosniff'
@@ -82,4 +111,3 @@ def apply_nosniff(response):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
